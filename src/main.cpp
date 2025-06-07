121be67816a750e25580ce26e65671c0e6bb2d35
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2025-06-08 00:01:03 +0700

    feat: add database connection to main

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,14 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include "PolygonLoader.h"
-#include "AirCraftManager.h"
+#include "AircraftManager.h"
+#include "FlightDatabaseManager.h"
 #include <QGeoCoordinate>
 #include <QTimer>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
-qRegisterMetaType<QGeoCoordinate>("QGeoCoordinate");
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
@@ -27,15 +28,19 @@ qRegisterMetaType<QGeoCoordinate>("QGeoCoordinate");
         Qt::QueuedConnection);
     PolygonLoader loader;
     AircraftModel aircraftModel;
+    FlightDatabaseManager dbManager;
     QList<QGeoCoordinate> hanoiPolygon = loader.loadHanoiPolygon("/home/domanhcuong/development/C++/QT/aircraft_hanoi/data/hanoi.json");
+    aircraftModel.setPolygon(hanoiPolygon);
+    aircraftModel.getStartAndEndPoint();
     engine.rootContext()->setContextProperty("hanoiPolygon", QVariant::fromValue(hanoiPolygon));
     engine.rootContext()->setContextProperty("aircraftModel", &aircraftModel);
     engine.load(url);
-
     QTimer timer;
     QObject::connect(&timer, &QTimer::timeout, [&]() {
         aircraftModel.updatePositions();
     });
     timer.start(1000);
+
+    //dbManager.connectToDatabase("localhost",5432,"aircraft","cuongceg","cuong1708");
     return app.exec();
 }
